9af1553ffe0a9159a38c6accfc8f1199d618d2b3
msm.mujahith <mujahith18862.new@gmail.com>
2025-05-23 15:58:39 +0530

    3rd commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+// use Illuminate\Container\Attributes\Storage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -18,4 +20,32 @@ class Post extends Model
         'category_id',
         'published_at'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+        
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function readTime()
+    {
+        $wordcount = str_word_count(strip_tags($this->content));
+        $minites = $wordcount / 100;
+        return max(1, $minites);
+    }
+
+    public function imageUrl()
+    {
+        if($this->image)
+        {
+            return Storage::url($this->image);
+        }
+        return null;
+
+    }
 }
--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,7 +1,8 @@
 <div class="flex bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-8">
 
     <div class="p-5 flex-1 ">
-        <a href="#">
+        <a href="{{ route('post.show',  [
+            'post'=> $post->slug, 'username'=>$post->user->username]) }}">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ Str::words($post->content), 15 }}</p>
