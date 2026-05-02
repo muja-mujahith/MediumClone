7d4fd8500cd3c92976d84ef5d6b148d7695872a8
msm.mujahith <mujahith18862.new@gmail.com>
2026-05-02 18:53:18 +0530

    fix the issues

--- docker/start.sh
+++ docker/start.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 set -e
 
-# Clear Laravel cache (VERY IMPORTANT)
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
-php artisan route:clear
 
-# Run migrations
 php artisan migrate --force
 
-# Start services
 php-fpm -D
 nginx -g "daemon off;"
