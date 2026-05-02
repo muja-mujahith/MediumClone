b602dca27e53bda7a22ba9a8969c18d6d1c3555b
msm.mujahith <mujahith18862.new@gmail.com>
2026-05-02 23:10:35 +0530

    fix the issue

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,9 @@ php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
 
+echo "--- Checking build assets ---"
+ls -la /var/www/html/public/build/
+
 php artisan migrate --force
 
 php-fpm -D
